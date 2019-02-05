b19db08d4a30351ec3180fc800278e3eda047945
HimaVijayan161 <47238474+HimaVijayan161@users.noreply.github.com>
2019-02-05 16:10:29 +0530

    new program

--- PracticePrograms/src/com/hima/automation/Armstrong.java
+++ PracticePrograms/src/com/hima/automation/Armstrong.java
@@ -3,8 +3,26 @@ package com.hima.automation;
 public class Armstrong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int originalNum=121;
+		int quotient=originalNum;
+		int remainder=0;
+		int cube=0;
+		while(quotient>0)
+		{
+			remainder=quotient%10;
+			quotient=quotient/10;
+			cube=cube+(remainder*remainder*remainder);
+		}
+		if(cube==originalNum)
+		{
+			System.out.println(originalNum+" is an armstrong");
+		}
+		else
+		{
+			System.out.println(originalNum+" is not an armstrong");
 
+		}
+			
 	}
 
 }
