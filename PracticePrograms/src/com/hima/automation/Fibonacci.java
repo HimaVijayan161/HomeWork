2f079147f28c11d6b91ac4282824f18309155cee
HimaVijayan161 <47238474+HimaVijayan161@users.noreply.github.com>
2019-02-06 16:20:20 +0530

    Fibonacci

--- PracticePrograms/src/com/hima/automation/Fibonacci.java
+++ PracticePrograms/src/com/hima/automation/Fibonacci.java
@@ -3,7 +3,19 @@ package com.hima.automation;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 11235813
+		
+		int i=1;int j=1;
+		int swapFact=1;
+		System.out.print(""+i+" "+j);
+		for(int k=1;k<=10;k++)
+		{
+			swapFact=i+j;
+			System.out.print(" "+swapFact);
+			j=i;
+			i=swapFact;
+		
+		}
 
 	}
 
