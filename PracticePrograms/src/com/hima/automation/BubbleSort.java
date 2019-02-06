ed8b444ed87d360af6476a66e8543bc088333b03
HimaVijayan161 <47238474+HimaVijayan161@users.noreply.github.com>
2019-02-06 17:48:39 +0530

    BubbleSort

--- PracticePrograms/src/com/hima/automation/BubbleSort.java
+++ PracticePrograms/src/com/hima/automation/BubbleSort.java
@@ -3,8 +3,18 @@ package com.hima.automation;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*The algorithm works by comparing each item in the list with the item next to it,
+		and swapping them if required. In other words, the largest element has bubbled to
+		the top of the array. The algorithm repeats this process until it makes a pass all
+		the way through the list without swapping any items.*/
 
+		
+		int a[]={2,5,3,4,1};
+		for(int k=0;k<=a.length;k++)
+		{
+			
+		}
+		
 	}
 
 }
