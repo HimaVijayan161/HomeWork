e534942eeb1db703c5a4475aa3e4aeffd9c1b868
HimaVijayan161 <47238474+HimaVijayan161@users.noreply.github.com>
2019-02-07 17:51:26 +0530

    BubbleSort

--- PracticePrograms/src/com/hima/automation/BubbleSort.java
+++ PracticePrograms/src/com/hima/automation/BubbleSort.java
@@ -10,9 +10,20 @@ public class BubbleSort {
 
 		
 		int a[]={2,5,3,4,1};
-		for(int k=0;k<=a.length;k++)
+		int temp=0;
+		for(int i=0;i<=a.length-1;i++)
 		{
-			
+			for(int j=i+1;j<=a.length-1;j++)
+			{
+				if(a[i]<a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;		
+
+				}
+			}
+			System.out.println(a[i]);
 		}
 		
 	}
